6691471ce61786e2907a356bf43f941a1b77a6d8
Shubhra Sarker <shuhra.sarker.dev@gmail.com>
2020-01-31 21:02:05 +0600

    Admin template fixed

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -26,16 +26,15 @@
                 <li class="nav-item has-treeview menu-open">
                     <a href="#" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Dashboard
+                        <p>Land Purchase
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="./index.html" class="nav-link">
+                            <a href="{{ route('land-purchase.create') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v1</p>
+                                <p>Add Land Purchase</p>
                             </a>
                         </li>
                         <li class="nav-item">
