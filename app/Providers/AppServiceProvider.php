01646a15500362ae3dac221ce02dfb32c6c632a0
Kamrul Islam Tushar <ki.tushar21@gmail.com>
2020-01-31 12:55:47 +0600

    Permission manager setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+
+    }
+
     /**
      * Register any application services.
      *
@@ -14,15 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-    }
+        
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
+Route::redirect('/', '/admin/home');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes(['register' => false]);
 
-Route::get('/', function () {
-    return view('welcome');
+// Change Password Routes...
+Route::get('change_password', 'Auth\ChangePasswordController@showChangePasswordForm')->name('auth.change_password');
+Route::patch('change_password', 'Auth\ChangePasswordController@changePassword')->name('auth.change_password');
+
+Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('permissions', 'Admin\PermissionsController');
+    Route::delete('permissions_mass_destroy', 'Admin\PermissionsController@massDestroy')->name('permissions.mass_destroy');
+    Route::resource('roles', 'Admin\RolesController');
+    Route::delete('roles_mass_destroy', 'Admin\RolesController@massDestroy')->name('roles.mass_destroy');
+    Route::resource('users', 'Admin\UsersController');
+    Route::delete('users_mass_destroy', 'Admin\UsersController@massDestroy')->name('users.mass_destroy');
 });
